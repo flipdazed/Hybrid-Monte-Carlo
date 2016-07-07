2ee97f415ce67e1953d962785dd46db28ada1653
Alexander McFarlane <alexander.mcfarlane@physics.org>
2016-07-07 01:23:14 +0100

    added cold start issue:#27

--- results/hmc_constEn_qho.py
+++ results/hmc_constEn_qho.py
@@ -9,11 +9,12 @@ pot = QHO()
 
 n, dim = 10, 1
 x0 = np.random.random((n,)*dim)
+x0 = np.zeros((n,)*dim)
 
 if '__main__' == __name__:
     hmc_constEn.main(x0, pot, file_name, save = False,
     n_samples=10, n_burn_in=0,
     step_size=0.1, n_steps=10,
-#    mixing_angle=0,
-#    accept_all = True,
+    mixing_angle=0,
+    accept_all = True,
     spacing=1.)
