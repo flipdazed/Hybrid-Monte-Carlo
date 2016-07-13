68d59b086c0c0d67d153e78f2137f8c1c232c65e
Alex McFarlane <alexander.mcfarlane@physics.org>
2016-07-13 18:53:44 +0100

    finished first draft of UWerr.m -> python NEEDS CHECKING! issue:#34

--- correlations/errors.py
+++ correlations/errors.py
@@ -34,7 +34,7 @@ def uWerr(samples, sample_obs, s_tau, quantity = 1, name = None, n_rep = None, *
     """
     
     # legacy: n_rep is set up as the number of entries in the data if reps = None
-    n = samples.size
+    n = float(samples.size)
     # primary_index = 1 # legacy - don't need
     primary = True      # legacy - don't need as we do all func evals before
     
@@ -53,26 +53,18 @@ def uWerr(samples, sample_obs, s_tau, quantity = 1, name = None, n_rep = None, *
     ### Computation of Gamma, automatic windowing
     # note: W=1,2,3,.. in Matlab-vectorindex <-> W=0,1,2,.. in the paper!
     
-    # legacy - set up some defaults incase the error analysis fails
-    v       = f_aav
-    v_diff  = 0.
-    v_ddiff = 0.
-    q_val   = []
-    itau        = 0.5
-    itau_diff   = 0.
-    
     # values for W=0:
-    normalisation = np.average(delpro**2.)
+    norm = (delpro**2).mean()
     
-    check.tryAssertNotEqual(normalisation, 0,
+    check.tryAssertNotEqual(norm, 0,
         msg = 'Normalisation cannot be zero: No fluctuations.' \
-        + '\nNormalisation: {}'.format(normalisation))
+        + '\nNormalisation: {}'.format(norm))
       
-    gamma_f_aav = [normalisation] # first element is the normalisation
+    acorr = [norm] # first element is the norm
     
     # compute Gamma(t), find the optimal W
     if s_tau == 0: # 0 = assumed absence of autocorrs
-        w_optimal   = 0.
+        w_best   = 0.
         t_max       = 0.
         flag        = False
     else:
@@ -80,8 +72,57 @@ def uWerr(samples, sample_obs, s_tau, quantity = 1, name = None, n_rep = None, *
         flag        = True
         g_int       = 0.
     
-    for t in in range(t_max):
-        gamma_f_i = 0.
+    for t in in range(1, t_max):
         # legacy:  a sum over the replica - can ignore as r=1 and just use:
-        gamma_f_i += delpro[:n-t]
+        # gamma_f_i += delpro[:n-t]*delpro[t:n] sucks in python syntax
+        # instead this can be replaced by a np.roll and slice off the end bits
+        # and then the mean can be taken all in one go
+        rickrolled = np.roll(delpro, t) # this totally wasn't a waste of a line of code
+        acorr_t = delpro*rickrolled[:n-t].mean() # bosh.
         
+        acorr.append(acorr_t) # append to gammas
+        
+        # this while section below can be improved drastically
+        ######
+        if flag: # this flag occurs if s_tau != 0 - should be explict
+            g_int += acorr/norm
+            if g_int <= 0: # calculate g_int
+                tau_w = np.finfo(float).eps
+            else:
+                tau_w = s_tau / np.log(1.+1./g_int)
+            g_w = np.exp(-t/tau_w) - tau_w/np.sqrt(t*n)
+            
+            # look for a sign switch in g_w
+            # optimal w occurs when sign changes
+            if g_w < 0:
+                w_best = t
+                t_max = min(t_max, 2*w_best)
+                flag = False #  now we want to calculate gamma up to tmax
+        ######
+    # end of loop over t
+    
+    check.tryAssertNotEqual(flag, False,
+        msg = 'Windowing condition failed up to W = {}'.format(t_max))
+    
+    acorr = np.asarray(acorr) # make into a np.ndarray for rest
+    
+    # this starting point is defined as Eq. 35 in the paper
+    g_sum = acorr(1:w_best).sum()
+    c_aav = norm + 2.*g_sum
+    
+    check.tryAssertLt(flag, g_sum,
+        msg = 'Estimated error^2 as sum(gamma) < 0...\n sum = {}'.format(g_sum)
+    
+    acorr  += c_aav/n                       # bias in Gamma corrected
+    c_aav = norm + 2*acorr(1:w_best).sum()  # refined estimate
+    sigma_f = np.sqrt(c_aav/n)              # error of F
+    acorrn  = acorr/norm                    # normalized autocorr.
+    itau_aav = np.cumsum(acorrn) - .5
+    
+    v       = f_aav
+    v_diff  = sigmaF
+    v_ddiff = dvalue*sqrt((w_best + .5)/n)
+    itau    = itau_aav(w_best + 1.)
+    itau_diff = int_tau*2*np.sqrt((w_best - int_tau + .5)/n)
+    
+    return v, v_diff, v_ddiff, itau, itau_diff
--- hmc/checks.py
+++ hmc/checks.py
@@ -83,18 +83,43 @@ def tryAssertEqual(val1, val2, error_msg):
     pass
 
 #
-def tryAssertLtEqual(lessthan, val2, error_msg):
-    """asserts that two values are equal
+def tryAssertLtEqual(lessthaneq, val2, error_msg):
+    """asserts that lessthaneq <= val2
     
     Required Inputs
-        val1        :: anything :: the first value to compare
+        lessthaneq  :: anything :: the supposedly smaller value / equal to
         val2        :: anything :: the second value to compare
         error_msg   :: string   :: message if error
     
     will check that val1 == val2
     """
     try:
-        assert lessthan <= val2
+        assert lessthaneq <= val2
+    except Exception, e:
+        # _, _, tb = sys.exc_info()
+        # traceback.print_tb(tb) # Fixed format
+        # tb_info = traceback.extract_tb(tb)
+        # filename, line, func, text = tb_info[-1]
+        # print 'line {} in {}'.format(line, text)
+        caller = getframeinfo(stack()[1][0])
+        print '\nError: Failed equal assertion...'
+        print '> {} : {} ~ {}\n'.format(caller.lineno, caller.filename, caller.function)
+        print error_msg
+        sys.exit(1)
+    pass
+#
+def tryAssertLt(lessthan, val2, error_msg):
+    """asserts that lessthan < val2
+    
+    Required Inputs
+        lessthan    :: anything :: the supposedly smaller value
+        val2        :: anything :: the second value to compare
+        error_msg   :: string   :: message if error
+    
+    will check that val1 == val2
+    """
+    try:
+        assert lessthan < val2
     except Exception, e:
         # _, _, tb = sys.exc_info()
         # traceback.print_tb(tb) # Fixed format
