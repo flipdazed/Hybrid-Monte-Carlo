25ef98dd17752d1c8f76f6be59878b09ca6d2d58
Alex McFarlane <alexander.mcfarlane@physics.org>
2016-07-18 04:21:02 +0100

    issue:#41 time wasting on SHO

--- results/hmc_sample_1d_sho.py
+++ results/hmc_sample_1d_sho.py
@@ -13,7 +13,7 @@ x0 = np.random.random((n,)*dim+(1,))
 x0 = np.asarray([[1.]])
 n_burn_in, n_samples = 15, 10000
 
-theory_label = r'$f(x) = \sqrt{{\frac{{\omega}}{{\pi}}}}e^{{-\omega x^2}}$ for $\omega=\frac{{1}}{{2}}$'
+theory_label = r'$f(x) = \sqrt{{\frac{{\omega\sqrt{{2}}}}{{2\pi}}}}e^{{-\frac{{\sqrt{{2}}\omega x^2}}{{2}}}}$ for $\omega^2=\frac{{5}}{{4}}$'
 def theory(x, samples):
     """The actual PDF curve
     
@@ -21,9 +21,10 @@ def theory(x, samples):
         x :: np.array :: 1D array of x-axis
         samples :: np.array :: 1D array of HMC samples
     """
-    w = 1.25
-    sigma_sq = 1./np.sqrt(2.*w)
-    return (None,), norm.pdf(x, 0, 0.79)
+    wsq = 1.25
+    sigma_sq = 1./np.sqrt(2.*wsq)
+    n = np.sqrt(np.sqrt(wsq*2)*.5/np.pi)
+    return (None,), n*np.exp(-np.sqrt(2*wsq)*.5*x**2)
 def fitted(x, samples):
     """The fitted PDF curve
     
@@ -36,7 +37,7 @@ def fitted(x, samples):
 #
 extra_data = [ # this passes fucntions to plot when smaple are obtained
     {'f':theory,'label':'Theory, {}'.format(theory_label)},
-    {'f':fitted,'label':r'Fitted $\mu = {:4.2f}$; $\sigma = {:4.2f}: \omega = \frac{{1}}{{2\sigma^2}}$'}]
+    {'f':fitted,'label':r'Fitted $\mu = {:4.2f}$; $\sigma = {:4.2f}: \sigma^2 = \frac{{1}}{{\sqrt{{2\omega^2}}}}$'}]
     
 y_label = r'Sampled Potential, $e^{-V(x)}$'
 
