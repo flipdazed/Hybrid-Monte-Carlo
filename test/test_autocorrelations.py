13dbb24bda8d4b154c828b70beb09546b29fb0f1
Alex McFarlane <alexander.mcfarlane@physics.org>
2016-08-06 08:53:53 +0100

    issue:#51 - unit test of fixed LTac
    Former-commit-id: 0c81bce9e67365d07791c5c07b2fb83db7b32cb5

--- test/test_autocorrelations.py
+++ test/test_autocorrelations.py
@@ -15,14 +15,22 @@ LOC_IN = "unittestLT_iAC.csv"
 INF  = 1e7
 ZERO = 1e-7
 
-def compareVals(columns_to_compare, tol):
+pd.options.display.float_format = '{:10,.4f}'.format
+
+def compareVals(df, columns_to_compare, tol):
     """Compares all values of columns from a given dataframe
     in the context of this unit test
     
     Required Inputs
+        df :: pd.DataFrame :: pandas dataframe with the data
         columns_to_compare :: list :: list of column names
         tol :: float :: tolerance
     """
+    passed = True
+    df[df<ZERO] = 0
+    df[df>INF] = np.nan
+    df.fillna(0)  # replace nans
+    
     all_combinations = itertools.combinations(columns_to_compare, 2)
     all_combinations = list(all_combinations)
     
@@ -33,7 +41,7 @@ def compareVals(columns_to_compare, tol):
         if not passed: 
             if a not in failed_df: failed_df.append(a)
             if b not in failed_df: failed_df.append(b)
-    return failed_df
+    return passed, failed_df
 
 class Fixed(object):
     """Runs tests for autocorrelation theory
@@ -49,7 +57,41 @@ class Fixed(object):
     def __init__(self, tol=ZERO, verbose = False):
         self.id  = 'Fixed Trajectory Lengths'
         self.tol = tol
+        self.m2fix = theory.M2_Fix()
+    
+    def laplaceTransformAutocorrelations(self, data_loc=LOC_DAT + LOC_FIX + LOC_LT, print_out=True):
+        """Compare the integrated autocorrelation calculations
+        Python vs. C++ vs. Mathematica derivations
         
+        Required Inputs
+            data_loc :: int :: LF trajectory lengths
+        """
+        passed = True
+        
+        # open the autocorrelation test data
+        df = pd.read_csv(data_loc)
+        df['ghmcLt cpp'] = df.apply(lambda r: ctheory.fixed.ghmcLt(
+            r['beta'], r['p_acc'], r['phi'], 1./r['tau'], r['theta']), axis=1)
+        df['lapTfm python'] = df.apply(lambda r: self.m2fix.lapTfm(
+            b=r['beta'], tau=r['tau'], m=r['phi']/r['tau'], pa=r['p_acc'], theta=r['theta']), axis=1)
+        
+        columns_to_compare = ["ghmcLt[... , 1/tau]",
+                                "ghmcLt cpp",
+                                "lapTfm python"]
+        
+        passed, failed_df = compareVals(df, columns_to_compare, self.tol)
+        
+        inputs = [u'beta', u'phi', u'theta', u'p_acc', u'tau']
+        failed_df = df[inputs + failed_df]
+        if print_out:
+            utils.display(self.id+': integrated autocorrelations', passed,
+                details = {
+                    'Inputs':columns_to_compare,
+                    'Failures':failed_df
+                    })
+        
+        return passed
+    
     def integratedAutocorrelations(self, data_loc=LOC_DAT + LOC_FIX + LOC_IN, print_out=True):
         """Compare the integrated autocorrelation calculations
         Python vs. C++ vs. Mathematica derivations
@@ -60,29 +102,26 @@ class Fixed(object):
         passed = True
         
         m2fix = theory.M2_Fix()
+        
         # open the autocorrelation test data
-        pd.options.display.float_format = '{:10,.4f}'.format
         df = pd.read_csv(data_loc)
-        df['ighmc cpp'] = df.apply(lambda row: ctheory.fixed.ighmc(row['p_acc'], row['phi'], row['theta']), axis=1)
-        df['ighmc python'] = df.apply(lambda row: m2fix.integrated(
-            tau=row['tau'], m=row['phi']/row['tau'], pa=row['p_acc'], theta=row['theta']), axis=1)
-        df[df<ZERO] = 0
-        df[df>INF] = np.nan
-        df.fillna(0)  # replace nans
+        df['ighmc cpp'] = df.apply(lambda r: ctheory.fixed.ighmc(r['p_acc'], r['phi'], r['theta']), axis=1)
+        df['ighmc python'] = df.apply(lambda r: self.m2fix.integrated(
+            tau=r['tau'], m=r['phi']/r['tau'], pa=r['p_acc'], theta=r['theta']), axis=1)
         
         columns_to_compare = ["A[phi, theta, p_acc]",
                                 "ghmcLt[0, ... , 1/tau]",
                                 "ighmc cpp",
                                 "ighmc python"]
         
-        failed_df = compareVals(columns_to_compare, self.tol)
+        passed, failed_df = compareVals(df, columns_to_compare, self.tol)
         
         inputs = ['phi', 'theta', 'p_acc', 'tau']
-        failed_df = df[inputs + failed_df].index(false)
+        failed_df = df[inputs + failed_df]
         if print_out:
             utils.display(self.id+': integrated autocorrelations', passed,
                 details = {
-                    'Inputs':[str(item[0]) +"\tvs.\t"+ str(item[1]) for item in all_combinations],
+                    'Inputs':columns_to_compare,
                     'Failures':failed_df
                     })
         
@@ -92,3 +131,4 @@ if __name__ == '__main__':
     
     fixed = Fixed()
     fixed.integratedAutocorrelations()
+    fixed.laplaceTransformAutocorrelations()
